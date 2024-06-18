d275d791bbc3226506f3765ba62883bd2a8077c2
vannyaVersalovic <150073317+vannyaVersalovic@users.noreply.github.com>
2024-06-17 23:51:36 -0400

    Servidor realizado correctamente

--- django/proyectoPaginaWeb/proyectoPaginaWeb/urls.py
+++ django/proyectoPaginaWeb/proyectoPaginaWeb/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from . import views # Importamos views (desde el mismo path)
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
