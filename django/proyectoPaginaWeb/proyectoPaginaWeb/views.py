81288b4208437b821c092d28ae3aebe4fc9a0715
vannyaVersalovic <150073317+vannyaVersalovic@users.noreply.github.com>
2024-06-17 23:46:15 -0400

    Creación de views.py
    Pilar fundamental para las vistas y sus rutas

---
+++ django/proyectoPaginaWeb/proyectoPaginaWeb/views.py
@@ -0,0 +1,24 @@
+# Ruta de este archvio proyectoPaginaWeb/proyectoPaginaWeb/views.py
+
+from django.shortcuts import render
+
+def inicio(request):
+    return render(request, 'proyectoPaginaWeb/Inicio.html')
+
+def hombre(request):
+    return render(request, 'proyectoPaginaWeb/Hombre.html')
+
+def infantil(request):
+    return render(request, 'proyectoPaginaWeb/Infantil.html')
+
+def mujer(request):
+    return render(request, 'proyectoPaginaWeb/Mujer.html')
+
+def nosotros(request):
+    return render(request, 'proyectoPaginaWeb/Nosotros.html')
+
+def oversize(request):
+    return render(request, 'proyectoPaginaWeb/Oversize.html')
+
+def videos(request):
+    return render(request, 'proyectoPaginaWeb/Videos.html')
